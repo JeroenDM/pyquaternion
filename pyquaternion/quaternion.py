74f5117e9fa7f678affac7a88adf9951d19d31ec
jeroendm <jeroendemaeyer@live.be>
2019-04-29 08:04:11 +0200

    add random_near function

--- pyquaternion/quaternion.py
+++ pyquaternion/quaternion.py
@@ -272,6 +272,27 @@ class Quaternion:
 
         return cls(q1, q2, q3, q4)
 
+    @classmethod
+    def random_near(cls, other, distance):
+        """ Generate random unit quaternion near a given quaternion.
+
+        Based on the 'SO3StateSpace' implementation in the open motion
+        planning library (http://ompl.kavrakilab.org/).
+
+        Params:
+            other: a Quaternion around wich to sample
+            distance: the maximum distance from the given Quaternion
+        """
+
+        if (distance > 0.25 * pi):
+            return cls.random()
+
+        d = np.random.random()
+        g1, g2, g3 = np.random.normal(size=(3,))
+        q_delta = Quaternion(axis=[g1, g2, g3], angle= (2 * d**(1/3)*distance))
+
+        return other * q_delta
+
     # Representation
     def __str__(self):
         """An informal, nicely printable string representation of the Quaternion object.
